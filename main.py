e565e7d97050b507d957b0f40d36c45f38b4a521
Charan <charan.reings125@gmail.com>
2025-01-27 15:40:09 +0530

    --test: returns 10 products on request to /about

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pandas as pd
+import sqlite3
+
+path = './prime_cart_products.db'
+conn = sqlite3.connect(path, check_same_thread=False)
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.post('/about' )
+def getTitle():
+    data = request.json
+    # data = json.loads(data)
+    title = data['title']
+    print(title)
+    df = pd.read_sql(f'select * from amazon_products where title like "%{title}%" limit 10', conn)
+    # conn.close()
+    print(len(df.to_dict(orient='records')))
+    # print(df.to_dict())
+    df = df.astype(object).to_dict()
+    products = []
+    for i in range(len(df['asin'])):
+        product = {key: df[key][i] for key in df}
+        products.append(product)
+    print(products)
+    
+    return jsonify(products)
+
+app.run('0.0.0.0', 5000, True)
+
